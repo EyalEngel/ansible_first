857d944fc95462674512ba9272a3165b78512530
EyalEngel <eyal075@gmail.com>
2018-07-20 01:41:26 -0700

    Created module chasis and documentation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ansible_first
+ansible_first is a challenge project.
+It's goal is to set up and configure an OpenLDAP server from scratch using Ansible,
+and writing a Python proprietary Ansible module that exhibits simple interactions with the server,
+such as adding/searching users.
+
+
+
+
+
+
+
+
+
+
+
+
+
+#(wish me luck :) )
--- python_script/first.py
+++ python_script/first.py
@@ -1,1 +1,25 @@
-#!/usr/bin/pythonconf
+#!/usr/bin/python
+
+from ansible.module_utils.urls import *
+
+# module does not support check mode... (yet)
+
+# note to self: make sure to add full support to LDAP protocol.
+def main():
+	module = AnsibleModule(
+		argument_spec = dict(
+			name = dict(required=True)				   # deleted will prob. not be supported
+			state = dict(choices=['added', 'deleted', 'searched'])       # serached will output a file
+			path = dict(required=False, default='~/')                    # should i use required?
+		)
+#		support_check_mode=True
+	)
+# Successful exit:
+#module.exit_json(changed=true, additional_data="Ansible is cool!")
+
+# Unsuccessful exit:
+#module.fail_json(msg="Something bad happend.")                    # msg is a must
+
+from ansible.module_utils.basic import *
+if __name__ == '__main__':
+	main()
