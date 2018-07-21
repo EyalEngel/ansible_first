7f855b5de800be502c1fde9b0ad87fbe124155b4
EyalEngel <eyal075@gmail.com>
2018-07-21 02:52:51 -0700

    Successfull connection. Added pip ldap3 to server dependencies

--- python_script/library/ldap.py
+++ python_script/library/ldap.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import *
+import ldap3
+
+
+import socket
+ADDRESS = socket.gethostname()                     # prob. not the best way, the best one i found.
+HOSTNAME, DOMAINNAME = ADDRESS.split('.')	   # assuming code will be run on every target machine.
+
+ADMIN_DN = "cn=Manager,dc=" + DOMAINNAME
+ADMIN_PASS = '1'
+
+
+
+
+
 
 DOCUMENTATION = '''
 module: ldap
@@ -34,22 +48,30 @@ EXAMPLES = '''
      state: exist
 '''
 
-#from ansible.module_utils.urls import *
 
 # module does not support check mode... (yet)
-
 # note to self: make sure to add full support to LDAP protocol.
+
+
+def connect(server=ADDRESS, user_dn=ADMIN_DN, passphrase=ADMIN_PASS):
+	# this function is used to connect and authenticate against the LDAP server.
+	# would normally write a "build_DN" function, but this is used only once. other DNs should come complete from input.	
+	
+	conn = ldap3.Connection(server, user_dn, passphrase, auto_bind=True)
+	return conn
+
 def main():
+
 	module = AnsibleModule(
 		argument_spec = dict(
-			name = dict(required=True),				      # deleted will prob. not be supported
-			state = dict(choices=['exist', 'absent', 'searched']),       # serached will output a file
-			path = dict(required=False, default='~/'),                    # should i use required?
-                        #server = dict(default=inventory_hostname)
+			name = dict(required=True),				                      # deleted will prob. not be supported
+			state = dict(required=True, choices=['present', 'absent', 'searched']),       # serached will output a file
+			path = dict(required=False, default='~/'),                                    # should i use required?
 		)
 #		support_check_mode=True
 	)
-	module.fail_json(msg="Something bad happend.") 
+	conn = connect()	
+	module.fail_json(msg="Something bad happend.\n" + str(conn))   # here to test output
 
 
 # Successful exit:
@@ -61,4 +83,5 @@ def main():
 #from ansible.module_utils.basic import *
 if __name__ == '__main__':
 	main()
+
 
